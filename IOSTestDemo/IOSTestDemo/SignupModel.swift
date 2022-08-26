32b912be515ebe25783a3b21f4ef0457f00c0eb5
shahvraj331 <shahvraj2016@gmail.com>
2022-08-26 09:51:25 +0530

    Base project setup

---
+++ IOSTestDemo/IOSTestDemo/SignupModel.swift
@@ -0,0 +1,14 @@
+//
+//  SignupModel.swift
+//  IOSTestDemo
+//
+//  Created by Vraj Shah on 26/08/22.
+//
+
+import Foundation
+
+struct SignupModel {
+    var name : String
+    var email : String
+    var password : String
+}
---
+++ IOSTestDemo/IOSTestDemo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  IOSTestDemo
+//
+//  Created by Vraj Shah on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
